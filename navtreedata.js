a8e3ada2989757bdd14a60eafba7e1c54f61d488
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-06 18:20:24 +0200

    Docs for deki-imu from deki-editor 9500697

--- navtreedata.js
+++ navtreedata.js
@@ -26,8 +26,7 @@ var NAVTREE =
 [
   [ "deki-imu", "index.html", [
     [ "Components", "index.html#autotoc_md0", null ],
-    [ "Feature flags", "index.html#autotoc_md1", null ],
-    [ "Component reference", "index.html#autotoc_md2", [
+    [ "Component reference", "index.html#autotoc_md1", [
       [ "LSM6DS3IMUComponent", "index.html#c-lsm6ds3imucomponent", null ]
     ] ]
   ] ]
